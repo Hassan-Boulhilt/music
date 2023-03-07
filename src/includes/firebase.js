e32ec6965b5e63f19aba597bcde7b30191d1e313
Hassan <hboulhilt@gmail.com>
2023-03-07 16:16:26 +0100

    nc

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -3,11 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvF17Y5ApfuVBSm_bemsZYoPrNVUT1r2s",
-  authDomain: "music-temp-2a2ab.firebaseapp.com",
-  projectId: "music-temp-2a2ab",
-  storageBucket: "music-temp-2a2ab.appspot.com",
-  appId: "1:513389509498:web:f976453109bb87de1034c3",
+  apiKey: "AIzaSyDGNeRGOsy3jTvEezGTQBv1G21w1QZ2oi0",
+  authDomain: "music-a87aa.firebaseapp.com",
+  projectId: "music-a87aa",
+  storageBucket: "music-a87aa.appspot.com",
+  messagingSenderId: "99388918538",
+  appId: "1:99388918538:web:3bbf011e62d5c17f0b88f3"
 };
 
 // Initialize Firebase
