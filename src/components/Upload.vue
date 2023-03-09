88c56b32dea776563befe5f4beb1ad3b7e4a84c6
Hassan <hboulhilt@gmail.com>
2023-03-09 22:02:56 +0100

    nc

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -19,7 +19,7 @@
           @dragover.prevent.stop="is_dragover = true"
           @dragenter.prevent.stop="is_dragover = true"
           @dragleave.prevent.stop="is_dragover = false"
-          @drop.prevent.stop="upload"
+          @drop.prevent.stop="upload($event)"
         >
           <h5>Drop your files here</h5>
         </div>
@@ -67,8 +67,10 @@ export default {
     };
   },
   methods: {
-    upload() {
+    upload(e) {
       this.is_dragover = false;
+      const { files } = e.dataTransfer;
+      console.log(files);
     },
   },
 };
